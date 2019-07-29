4b43ca080a6098b86ebf2a900ecb33fae36ac56c
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-07-29 09:21:52 +0700

    update configuration

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from '../config/config';
+import {BASE_URL, SERVER_URL} from '../config/config';
 import APIService from './APIService';
 
 const getUserDetail = async function(user) {
--- src/utils/logs.js
+++ src/utils/logs.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from "../config/config";
+import {BASE_URL, SERVER_URL} from "../config/config";
 
 export function postLog(category, activity, value) {
   axios
