90dc6da129cc0b61eb33fc167c01f0ab7d495575
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2019-09-24 19:34:00 +0700

    fixing quiz

--- src/pages/siswa/Quiz.jsx
+++ src/pages/siswa/Quiz.jsx
@@ -4,11 +4,16 @@ import classnames from 'classnames'
 import Card from "../../components/UI/Card";
 import ProgressBar from "../../components/UI/ProgressBar";
 
-const Choice = ({value, text, onClickChoice, setChoiceActive}) => {
+const Choice = ({value, text,  selected, onClick}) => {
+  const color = (selected) ? 'green' : '#E5E5E5'; //jika selected === true maka warna border menjadi green
+
   return (
     <div className="card" 
-        style={{borderRadius:"10px",marginTop:"10px", border: `2px solid #E5E5E5`, padding:"10px"}}
-        onClick={() => setChoiceActive(value)}>
+        style={{borderRadius:"10px",marginTop:"10px", border: `2px solid ${color}`, padding:"10px"}}
+         onClick={() => {
+            onClick(value); // memanggil fungsi onClick dengan parameter value (jawaban :a,b,c,d)
+         }}
+    >
         <h6>{text}</h6>
     </div>
   )
@@ -17,20 +22,27 @@ const Choice = ({value, text, onClickChoice, setChoiceActive}) => {
 const Quiz = ({ className }) => {
   const [choiceActive,setChoiceActive] = useState('a');
 
+  const onChoiceClick = (value) => {
+    //value : jawaban=> a,b,c,d
+    setChoiceActive(value);
+  }
+
   return (
     <Card className={classnames(className,"card col-10 offset-1")}>
       <ProgressBar/>
       <div className="col-8 offset-2" style={{marginTop: "10px"}}>
         <h3 style={{height: "100px"}}>Jawablah pertanyaan ini ? {choiceActive}</h3>
         <div>
-        <Choice value="a" text="Jawaban A" onClickChoice={choiceActive === 'a' ? true : false} setChoiceActive/>
-        <Choice value="b" text="Jawaban B" onClickChoice={choiceActive === 'b' ? true : false} setChoiceActive/>
-        <Choice value="c" text="Jawaban C" onClickChoice={choiceActive === 'c' ? true : false} setChoiceActive/>
-        <Choice value="d" text="Jawaban D" onClickChoice={choiceActive === 'd' ? true : false} setChoiceActive/>
+        <Choice value="a" text="Jawaban A" selected={choiceActive === 'a' ? true : false} onClick={onChoiceClick}/>
+        <Choice value="b" text="Jawaban B" selected={choiceActive === 'b' ? true : false} onClick={onChoiceClick} />
+        <Choice value="c" text="Jawaban C" selected={choiceActive === 'c' ? true : false} onClick={onChoiceClick} />
+        <Choice value="d" text="Jawaban D" selected={choiceActive === 'd' ? true : false} onClick={onChoiceClick} />
           <button 
               className="btn btn-primary float-right" 
-              style={{marginTop:"20px", textAlign:"center"}} >
-                Periksa 
+              style={{marginTop:"20px", textAlign:"center"}} onClick={() => {
+                //pemeriksaan jawaban choiceActive === jawaban benar
+          }}>
+                Periksa
           </button>
         </div>
       </div>
